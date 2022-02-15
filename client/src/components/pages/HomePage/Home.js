d8cba0e3ed6ac81711906a16c2e8e7e4de34c1ad
Konnor-Biats <biatsdigitalmedia@gmail.com>
2022-02-15 11:11:31 -0500

    Worked on Dog Section

--- client/src/components/pages/HomePage/Home.js
+++ client/src/components/pages/HomePage/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroSection from '../../HeroSection'
 import {homeObjOne, homeObjTwo} from './Data'
+import DogCards from '../../DogCards';
 
 function Home() {
   return (
     <>
         <HeroSection {...homeObjOne} />
+        <DogCards />
         <HeroSection {...homeObjTwo} />
 
     </>
